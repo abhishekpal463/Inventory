2d44f05893586c31ce1f9141a50b64842b10b923
Abhishek Pal <abhishekpal463@gmail.com>
2021-12-17 19:42:04 +0530

    updation to run app.js

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ mongoose.connect(
 )
 .catch(err => console.log(err));
 
+app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 app.use(express.json());//parsing the application/json
 app.use(express.urlencoded({ extended: false }));//parses the x-www-form-urlencoded
